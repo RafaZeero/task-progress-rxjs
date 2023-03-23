3ce3f2608bff11f4ba590ee08f38ea87474a5c00
RafaZeero <rafaa99@gmail.com>
2023-03-23 20:39:47 -0300

    feat: create first observable components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'task-progress-rxjs';
+  public title = 'task-progress-rxjs';
+
+  public taskStarts = new Observable();
+  public taskCompletions = new Observable();
+  public showSpinner = new Observable();
 }
