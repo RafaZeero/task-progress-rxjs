81ab3c39e563224dddc9db17b0606c43faacd4fa
RafaZeero <rafaa99@gmail.com>
2023-03-23 20:46:50 -0300

    feat: pipe events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, merge, Observable, scan } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,28 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   public title = 'task-progress-rxjs';
 
+  /**
+   * Start counting from 0
+   * When an Async task starts, count +1
+   * When an Async task completes, coun -1
+   */
+
+  public startingValue = 0;
   public taskStarts = new Observable();
   public taskCompletions = new Observable();
   public showSpinner = new Observable();
+
+  public loadUp = this.taskStarts.pipe(map(() => 1));
+  public loadDown = this.taskCompletions.pipe(map(() => -1));
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx //
+  // Events:
+
+  public loadVariation = merge(this.loadUp, this.loadDown);
+  public currentLoadCount = this.loadVariation.pipe(
+    scan(
+      (totalCurrentLoads, changeInLoads) => totalCurrentLoads + changeInLoads,
+      this.startingValue
+    )
+  );
 }
