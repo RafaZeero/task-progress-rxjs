a343700cabfdc2e7e52f1643fbd92e2353dcf690
RafaZeero <rafaa99@gmail.com>
2023-04-01 00:30:52 -0300

    feat: testing merge map

--- src/app/hoc-maps/hoc-maps.component.ts
+++ src/app/hoc-maps/hoc-maps.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { interval, map, mergeMap, Observable, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-hoc-maps',
@@ -7,8 +8,32 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './hoc-maps.component.html',
   styleUrls: ['./hoc-maps.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HocMapsComponent {
+  public concat$: any;
+  public merge$!: Observable<any>;
 
+  public requestOne$ = interval(1000).pipe(take(2));
+  public requestTwo$ = interval(2000).pipe(take(3));
+
+  public ngOnInit() {
+    this.mergeMapOperator();
+  }
+
+  public mergeMapOperator() {
+    const mergeMapOp = this.requestOne$.pipe(
+      mergeMap(() => this.requestTwo$),
+      // Get value from requestOne (x) and multiply by requestTwo (y)
+      map((x, y) => x * y)
+    );
+
+    this.merge$ = mergeMapOp;
+
+    mergeMapOp.subscribe({
+      next: (x) => console.log('next ' + x),
+      error: (err) => console.log('error ' + err),
+      complete: () => console.log('done'),
+    });
+  }
 }
