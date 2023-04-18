28a4e6e5acece4d81308a0b0ba8c41d76beafaa7
RafaZeero <rafaa99@gmail.com>
2023-04-17 22:22:36 -0300

    feat: avoid using subject when unnecessary

--- src/app/common-mistakes/common-mistakes.component.ts
+++ src/app/common-mistakes/common-mistakes.component.ts
@@ -1,5 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  Observable,
+  Subject,
+  interval,
+  merge,
+  switchMap,
+  takeUntil,
+} from 'rxjs';
+import { SubscribeService } from '../services';
 
 @Component({
   selector: 'app-common-mistakes',
@@ -7,8 +21,29 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './common-mistakes.component.html',
   styleUrls: ['./common-mistakes.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommonMistakesComponent {
+export class CommonMistakesComponent implements OnInit {
+  public subscriptionService = inject(SubscribeService);
 
+  // public click = new Subject<MouseEvent>();
+
+  public click$ = new Observable((observer) => {
+    const listener = (e: MouseEvent) => observer.next(e.clientX);
+    document.addEventListener('click', listener);
+
+    return () => document.removeEventListener('click', listener);
+  });
+
+  public count$ = interval(1000);
+
+  public ngOnInit(): void {
+    const sub = merge(this.click$, this.count$).subscribe(
+      this.subscriptionService.subFn('For each click')
+    );
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    }, 4000);
+  }
 }
