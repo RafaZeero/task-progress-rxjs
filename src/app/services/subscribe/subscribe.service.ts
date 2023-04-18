bb666ac6e2fed24c6eeacf6205482bf5205226d0
RafaZeero <rafaa99@gmail.com>
2023-04-17 22:22:31 -0300

    feat: create subscribe service

---
+++ src/app/services/subscribe/subscribe.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SubscribeService {
+  public subFn = (message?: string) => ({
+    next: (x: any) => console.log(message, x),
+    error: (err: any) => console.log('error: ' + err),
+    complete: () => console.log('done'),
+  });
+
+  constructor() {}
+}
