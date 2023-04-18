d55a6926ac222f8810b0cc9992d6cdf012be0223
RafaZeero <rafaa99@gmail.com>
2023-04-17 22:58:29 -0300

    feat: differences between zip and combineLatest operators

--- src/app/common-mistakes/common-mistakes.component.ts
+++ src/app/common-mistakes/common-mistakes.component.ts
@@ -8,15 +8,19 @@ import { CommonModule } from '@angular/common';
 import {
   Observable,
   Subject,
+  combineLatest,
   from,
   fromEvent,
   interval,
   map,
   merge,
+  of,
   scan,
   switchMap,
+  take,
   takeUntil,
   timer,
+  zip,
 } from 'rxjs';
 import { SubscribeService } from '../services';
 
@@ -38,14 +42,19 @@ export class CommonMistakesComponent implements OnInit {
     //   .pipe(takeUntil(this.four$))
     //   .subscribe(this.subscriptionService.subFn('For each click'));
     // this.count$.subscribe(this.subscriptionService.subFn());
-    this.tickWhenClick$.subscribe(this.subscriptionService.subFn());
+    // this.tickWhenClick$
+    //   .pipe(take(20))
+    //   .subscribe(this.subscriptionService.subFn());
+
+    this.volume$.subscribe(this.subscriptionService.subFn('Non zip'));
+    this.volumeZip$.subscribe(this.subscriptionService.subFn('Zip'));
   }
 
-  public xCoordFromClick$ = fromEvent(document, 'click').pipe(
-    map((v) => (v as MouseEvent).clientX)
-  );
+  // public xCoordFromClick$ = fromEvent(document, 'click').pipe(
+  //   map((v) => (v as MouseEvent).clientX)
+  // );
 
-  public click$ = fromEvent(document, 'click');
+  // public click$ = fromEvent(document, 'click');
   // public count$ = interval(1000);
   // public four$ = timer(4000);
 
@@ -64,5 +73,19 @@ export class CommonMistakesComponent implements OnInit {
   //   scan((acc, x) => acc + x, 0)
   // );
 
-  public tickWhenClick$ = this.click$.pipe(switchMap(() => interval(500)));
+  // public tickWhenClick$ = this.click$.pipe(switchMap(() => interval(500)));
+
+  public length$ = of(5);
+  public width$ = of(7);
+  public height$ = of(2.5, 7.3);
+
+  public volume$ = combineLatest([
+    this.length$,
+    this.width$,
+    this.height$,
+  ]).pipe(map(([l, w, h]) => l * w * h));
+
+  public volumeZip$ = zip([this.length$, this.width$, this.height$]).pipe(
+    map(([l, w, h]) => l * w * h)
+  );
 }
