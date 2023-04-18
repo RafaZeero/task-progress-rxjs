a460b15d841b9963ec0999c96f246e34154a8f57
RafaZeero <rafaa99@gmail.com>
2023-04-17 22:50:23 -0300

    feat: creates new stream whenever a click event in DOM

--- src/app/common-mistakes/common-mistakes.component.ts
+++ src/app/common-mistakes/common-mistakes.component.ts
@@ -37,28 +37,32 @@ export class CommonMistakesComponent implements OnInit {
     // merge(this.click$, this.count$)
     //   .pipe(takeUntil(this.four$))
     //   .subscribe(this.subscriptionService.subFn('For each click'));
-
-    this.count$.subscribe(this.subscriptionService.subFn());
+    // this.count$.subscribe(this.subscriptionService.subFn());
+    this.tickWhenClick$.subscribe(this.subscriptionService.subFn());
   }
 
-  public click$ = fromEvent(document, 'click').pipe(
+  public xCoordFromClick$ = fromEvent(document, 'click').pipe(
     map((v) => (v as MouseEvent).clientX)
   );
+
+  public click$ = fromEvent(document, 'click');
   // public count$ = interval(1000);
-  public four$ = timer(4000);
+  // public four$ = timer(4000);
 
   // 1. Identify source of data
   // 2. Convert to Observables
   // 3. Compose
 
-  public res$ = from(
-    fetch('https://jsonplaceholder.typicode.com/users/1').then((res) =>
-      res.json()
-    )
-  );
+  // public res$ = from(
+  //   fetch('https://jsonplaceholder.typicode.com/users/1').then((res) =>
+  //     res.json()
+  //   )
+  // );
 
-  public count$ = merge(this.click$, this.res$).pipe(
-    map(() => 1),
-    scan((acc, x) => acc + x, 0)
-  );
+  // public count$ = merge(this.click$, this.res$).pipe(
+  //   map(() => 1),
+  //   scan((acc, x) => acc + x, 0)
+  // );
+
+  public tickWhenClick$ = this.click$.pipe(switchMap(() => interval(500)));
 }
